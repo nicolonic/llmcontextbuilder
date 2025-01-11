9dd100d5ccd5e3ab34775b5ed55644f36cdf9e51
Recklesz <8067269+Recklesz@users.noreply.github.com>
2025-01-11 15:34:38 +0000

    Added os import and dynamic port; added service config file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from flask import Flask, render_template
+import os
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
@@ -17,4 +18,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
